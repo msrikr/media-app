d08c09f3ac5ccc71efd5038475e9e40e20744e60
msrikr <sr7847435@gmail.com>
2025-01-30 21:17:08 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('register').addEventListener('click', () => {
+    window.location.href = "/register.html";
+});
+
+document.getElementById('login').addEventListener('click', () => {
+    window.location.href = "/login.html";
+});
